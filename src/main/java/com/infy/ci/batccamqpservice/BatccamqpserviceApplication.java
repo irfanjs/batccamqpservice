b77b6169456063f7ffb3a5c06b3532f8af4be8e3
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-27 16:07:42 +0530

    first commit

---
+++ src/main/java/com/infy/ci/batccamqpservice/BatccamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.batccamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatccamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatccamqpserviceApplication.class, args);
+	}
+
+}
